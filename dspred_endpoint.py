2bb44da34dde1a006993ef972172115ef28b04a6
Ryan Zhang <zyratlo@gmail.com>
2024-01-27 13:46:32 -0800

    added getters to api

--- dspred_endpoint.py
+++ dspred_endpoint.py
@@ -15,6 +15,18 @@ with open(os.path.join("models", "random_forest_model.pkl"), "rb") as file:
 with open(os.path.join("models", "symptoms.pkl"), "rb") as file:
     symptoms = pickle.load(file)
 
+# Load illness descriptions
+with open(os.path.join("models", "illness_descriptions.pkl"), "rb") as file:
+    illness_descriptions = pickle.load(file)
+
+# Load id conversion
+with open(os.path.join("models", "convert_id_to_illness.pkl"), "rb") as file:
+    convert_id_to_illness = pickle.load(file)
+
+# Load illness precautions
+with open(os.path.join("models", "illness_precautions.pkl"), "rb") as file:
+    illness_precautions = pickle.load(file)
+
 # Define the input data model
 class InputData(BaseModel):
     features: List[bool]
@@ -22,7 +34,7 @@ class InputData(BaseModel):
 @app.post("/predict")
 def predict(data: InputData):
     if (len(data.features) != 131):
-        raise HTTPException(status_code=500, detail="Input data needs to be a list of 131 boolean values")
+        raise HTTPException(status_code=422, detail="Input data needs to be a list of 131 boolean values")
 
     try:
         # Extract features from the input data
@@ -31,10 +43,22 @@ def predict(data: InputData):
         # Make predictions using the loaded model
         prediction = model.predict(features)
 
-        return {"prediction": prediction.tolist()}
+        return {"prediction": convert_id_to_illness[prediction[0]]}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
     
 @app.post("/get_symptoms")
 def get_symptoms():
     return symptoms
+
+@app.post("/get_illness_description")
+def get_illness_description(illness: str):
+    if (illness not in illness_descriptions.keys()):
+        raise HTTPException(status_code=422, detail="Illness is not contained in this database")
+    return illness_descriptions[illness]
+
+@app.post("/get_precautions")
+def get_precautions(illness: str):
+    if (illness not in illness_precautions.keys()):
+        raise HTTPException(status_code=422, detail="Illness is not contained in this database")
+    return list(illness_precautions[illness])
